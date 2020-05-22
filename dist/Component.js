00eeabba71b424250f968c257ffbfd118e59a976
Erika Jonell <erika.jonell@gmail.com>
2020-05-22 15:30:32 -0400

    ......

--- dist/Component.js
+++ dist/Component.js
@@ -1,15 +1,19 @@
-export class Component {
-    constructor() {
-        this.owner = -1;
+let Component = /** @class */ (() => {
+    class Component {
+        constructor() {
+            this.owner = -1;
+        }
+        get type() {
+            let inst = this.constructor;
+            return inst.type;
+        }
+        set type(value) {
+            let inst = this.constructor;
+            inst.type = value;
+        }
     }
-    get type() {
-        let inst = this.constructor;
-        return inst.type;
-    }
-    set type(value) {
-        let inst = this.constructor;
-        inst.type = value;
-    }
-}
-Component.type = -1;
+    Component.type = -1;
+    return Component;
+})();
+export { Component };
 //# sourceMappingURL=Component.js.map
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,15 +1,12 @@
-declare module 'ecsf';
-// declare module 'ecsf' {
-  // export * as Bag from 'Bag';
-  // export * as Component from 'Component';
-  // export * as ComponentManager from 'ComponentManager';
-  // export * as ComponentMapper from 'ComponentMapper';
-  // export * as EcsInstance from 'EcsInstance';
-  // export * as Entity from 'Entity';
-  // export * as EntityManager from 'EntityManager';
-  // export * as EntitySystem from 'EntitySystem';
-  // export * as GroupManager from 'GroupManager';
-  // export * as SystemManager from 'SystemManager';
-  // export * as TagManager from 'TagManager';
-// }
-
+export * from './Bag';
+export * from './Component';
+export * from './ComponentManager';
+export * from './ComponentMapper';
+export * from './EcsInstance';
+export * from './Entity';
+export * from './EntityManager';
+export * from './EntitySystem';
+export * from './GroupManager';
+export * from './SystemManager';
+export * from './TagManager';
+//# sourceMappingURL=index.d.ts.map
