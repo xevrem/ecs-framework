103a80b62770e9d40b9c5a0c4c8698064762e67a
Erika Jonell <erika.jonell@gmail.com>
2020-05-22 15:09:05 -0400

    arggg

--- dist/Entity.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class Entity {
-    id: number;
-}
-//# sourceMappingURL=Entity.d.ts.map
--- dist/TagManager.d.ts
+++
@@ -1,9 +0,0 @@
-import { Entity } from './Entity';
-export declare class TagManager {
-    private __tags;
-    getEntityByTag(tag: string): Entity;
-    tagEntity(tag: string, entity: Entity): void;
-    deleteEntity(entity: Entity): void;
-    cleanUp(): void;
-}
-//# sourceMappingURL=TagManager.d.ts.map
